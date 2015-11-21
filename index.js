d37f97bc52bc12cf0d73891e4d70a4aca2f398bf
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-21 23:46:17 +0900

    Add module interface

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var subclass = require('subclass')
+
+var CommandExecutionService = require('./lib/CommandExecutionService')
+
+/**
+ * The module interface of Brutus
+ *
+ * @class
+ */
+var Brutus = subclass(function (pt) {
+
+    /**
+     * @param {Object} options The options
+     * @param {String} options.name The name of the command
+     * @param {Object} options.options The command options
+     * @param {CommandDelegate} delegate The command delegate
+     */
+    pt.constructor = function (options) {
+
+        if (!(this instanceof Brutus)) {
+
+            return new Brutus(options)
+
+        }
+
+        this.cmdService = new CommandExecutionService(options.name, options.options, options.delegate)
+
+    }
+
+    /**
+     * Executes the command as a cli
+     */
+    pt.execAsCli = function () {
+
+        this.cmdService.executeAsCli()
+
+    }
+
+    /**
+     * Executes the command without using cli options.
+     */
+    pt.exec = function () {
+
+        this.cmdService.execute()
+
+    }
+
+})
+
+module.exports = Brutus
